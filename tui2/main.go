cfb9135346aa3d18bd8aebc6a694d1a10b8795e8
Stefan Schroeder <ondekoza@gmail.com>
2023-11-26 23:39:47 +0100

    no dialog

--- tui2/main.go
+++ tui2/main.go
@@ -168,6 +168,7 @@ func (m *model) MoveEntryToState(s string) {
 			current_table_index, _ := strconv.Atoi(current_table_row[0])
 
 			m.btd[current_table_index].State = s
+			// Moved to new state. Ask for date.
 
 			titles := m.btd.GetTitles()
 			m.table = m.build_table(titles, m.table.Cursor(), m.Tabs[m.activeTab])
@@ -414,6 +415,51 @@ func (m model) View() string {
 		doc.WriteString(windowStyle.Width(4 + (lipgloss.Width(row) - windowStyle.GetHorizontalFrameSize())).Render("abc"))
 	}
 
+
+/*
+
+	width := 96
+
+	dialogBoxStyle := lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color("#874BFD")).
+			Padding(1, 0).
+			BorderTop(true).
+			BorderLeft(true).
+			BorderRight(true).
+			BorderBottom(true)
+
+	buttonStyle := lipgloss.NewStyle().
+			Foreground(lipgloss.Color("#FFF7DB")).
+			Background(lipgloss.Color("#888B7E")).
+			Padding(0, 3).
+			MarginTop(1)
+
+	activeButtonStyle := buttonStyle.Copy().
+				Foreground(lipgloss.Color("#FFF7DB")).
+				Background(lipgloss.Color("#F25D94")).
+				MarginRight(2).
+				Underline(true)
+
+	subtle    := lipgloss.AdaptiveColor{Light: "#D9DCCF", Dark: "#383838"}
+	{
+		okButton := activeButtonStyle.Render("Yes")
+		cancelButton := buttonStyle.Render("Maybe")
+
+		question := lipgloss.NewStyle().Width(50).Align(lipgloss.Center).Render("Are you sure you want to eat marmalade?")
+		buttons := lipgloss.JoinHorizontal(lipgloss.Top, okButton, cancelButton)
+		ui := lipgloss.JoinVertical(lipgloss.Center, question, buttons)
+
+		dialog := lipgloss.Place(width, 9,
+			lipgloss.Center, lipgloss.Center,
+			dialogBoxStyle.Render(ui),
+			lipgloss.WithWhitespaceChars("猫咪"),
+			lipgloss.WithWhitespaceForeground(subtle),
+		)
+
+		doc.WriteString(dialog + "\n\n")
+	}
+*/
 	/*if m.focusIndex == 0 {
 		doc.WriteString("\nEnter task")
 	}
